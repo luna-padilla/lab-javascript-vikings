fe1b2aea7d3c290bc5c57fc20c9e5e8fc3d17267
luna <lunapadilla.8@gmail.com>
2024-10-06 17:03:30 +0200

    add solutions

--- src/viking.js
+++ src/viking.js
@@ -27,11 +27,66 @@ class Viking extends Soldier {
       return `${this.name} has died in act of combat`;
     }
   }
-  battleCry() {}
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+  vikingAttack() {
+    const randomSaxonIndice = Math.floor(Math.random() * this.saxonArmy.length);
+    const soldierSaxon = this.saxonArmy[randomSaxonIndice];
+
+    const randomVikingIndice = Math.floor(
+      Math.random() * this.vikingArmy.length
+    );
+    const soldierViking = this.vikingArmy[randomVikingIndice];
+
+    const msg = soldierSaxon.receiveDamage(soldierViking.attack());
+    if (soldierSaxon.health <= 0) {
+      this.saxonArmy.splice(randomSaxonIndice, 1);
+    }
+
+    return msg;
+  }
+  saxonAttack() {
+    const randomSaxonIndice = Math.floor(Math.random() * this.saxonArmy.length);
+    const soldierSaxon = this.saxonArmy[randomSaxonIndice];
+
+    const randomVikingIndice = Math.floor(
+      Math.random() * this.vikingArmy.length
+    );
+    const soldierViking = this.vikingArmy[randomVikingIndice];
+
+    const msg = soldierViking.receiveDamage(soldierSaxon.attack());
+    if (soldierViking.health <= 0) {
+      this.vikingArmy.splice(randomVikingIndice, 1);
+    }
+
+    return msg;
+  }
+  showStatus() {}
+}
